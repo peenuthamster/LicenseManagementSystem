48657880eb6a14599d93d77878ddf59f16eff47d
Saurabh Lama <saurabhllamaa@gmail.com>
2025-02-07 00:42:47 +0545

    second commit

--- src/main/LicenseManagementSystem.java
+++ src/main/LicenseManagementSystem.java
@@ -1,21 +1,26 @@
 package main;
 
+import users.User;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LicenseManagementSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int choice;
+        ArrayList<User> users = new ArrayList<User>();
+
         System.out.println("License Management System");
         System.out.println("1. Register");
         System.out.println("2. Login");
         System.out.println("3. Exit");
 
-        int choice;
         do {
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-
+                    registerUser(sc);
                     break;
                 case 2:
                     break;
@@ -25,5 +30,28 @@ public class LicenseManagementSystem {
                     System.out.println("Invalid choice, please try again");
             }
         } while (choice != 3);
+    }
+
+    private static void registerUser(Scanner sc) {
+        System.out.println("\n------User Registration------");
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your email");
+        String email = sc.nextLine();
+
+        System.out.println("Enter your citizenship number");
+        String citizenship = sc.nextLine();
+
+        System.out.println("Enter your password");
+        String password = sc.nextLine();
+
+        System.out.println("Enter your role");
+        String role = sc.nextLine().toLowerCase();
+
+//        User newUser = new User(name, email, citizenship, password, role);
+//        users.add(newUser);
+
+        System.out.println("Registration Successful! Welcome " + newUser.getName());
     }
 }
--- src/users/User.java
+++ src/users/User.java
@@ -8,12 +8,28 @@ public class User {
     String citizenshipNumber;
     String password;
 
-    public User(int userID, String name, String role, String email, String citizenshipNumber, String password) {
-        this.userID = userID;
+    public User(String name, String role, String email, String citizenshipNumber, String password) {
         this.name = name;
         this.role = role;
         this.email = email;
         this.citizenshipNumber = citizenshipNumber;
         this.password = password;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public String getCitizenshipNumber(){
+        return citizenshipNumber;
+    }
+
 }
